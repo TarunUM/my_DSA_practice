003d813e5b420145b88b0ebd1c7598fcf6d8595f
TarunM <tarun.8792@gmail.com>
2023-01-09 03:10:32 +0530

    adding rest

---
+++ src/Leet/DP/JumpGame.java
@@ -0,0 +1,12 @@
+package Leet.DP;
+//https://leetcode.com/problems/jump-game/
+
+public class JumpGame {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean canJump(int[] nums) {
+        return true;
+    }
+}
---
+++ src/Leet/DP/LongestIncreasingSubsequence.java
@@ -0,0 +1,11 @@
+package Leet.DP;
+
+public class LongestIncreasingSubsequence {
+    public static void main(String[] args) {
+
+    }
+
+    public static int lengthOfLIS(int[] nums) {
+        return 1;
+    }
+}
