803491a43a2668e47d084ca64fdc07663aa3d0c8
TarunM <tarun.8792@gmail.com>
2023-04-19 16:46:14 +0530

    OOPs - encapsulation And abstraction

--- src/Leet/CanPlaceFlowers.java
+++ src/Leet/CanPlaceFlowers.java
@@ -1,13 +1,22 @@
 package Leet;
 
-import java.util.Date;
+import java.util.ArrayList;
 
 //605 = https://leetcode.com/problems/can-place-flowers/
 public class CanPlaceFlowers {
 
     public static void main(String[] args) {
         int[] flowerbed = new int[]{1,0,0,0,1};
-        System.out.println(canPlaceFlowers(flowerbed,1).toString());
+        ArrayList<Integer> flowerbed2 = new ArrayList<>();
+        flowerbed2.add(1);
+        flowerbed2.add(0);
+        flowerbed2.add(0);
+        flowerbed2.add(0);
+        flowerbed2.add(1);
+        System.out.println(canPlaceFlowers(flowerbed,2).toString());
+        System.out.println(canPlaceFlowers2(flowerbed2,2).toString());
+
+
     }
     public static returnData canPlaceFlowers(int[] flowerbed, int n) {
         long start = System.nanoTime();
@@ -15,6 +24,24 @@ public class CanPlaceFlowers {
         int len = flowerbed.length;
         while(i<len && n > 0){
             if((i==0 || flowerbed[i-1] == 0) && flowerbed[i] == 0 && (i == (len-1) || flowerbed[i+1] == 0)){
+                i++;
+                n--;
+            }
+            i++;
+        }
+        long end = System.nanoTime();
+        return new returnData((end - start), n == 0);
+    }
+
+    public static returnData canPlaceFlowers2(ArrayList<Integer> flowerbed, int n) {
+        long start = System.nanoTime();
+        int i = 1;
+        int len = flowerbed.size() - 1;
+
+        flowerbed.add(0, 0);
+
+        while(i<len && n > 0){
+            if(flowerbed.get(i - 1) == 0 && flowerbed.get(i) == 0 && flowerbed.get(i + 1) == 0){
                 i++;
                 n--;
             }
